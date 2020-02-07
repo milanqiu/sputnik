76518389aaee905e353ed3e193882481559184e9
Milan Qiu <product@milanqiu.net>
2020-02-07 15:33:02 +0800

    Add encoding solution into web

---
+++ sputnik-web/src/main/java/net/milanqiu/sputnik/CharsetFilter.java
@@ -0,0 +1,28 @@
+package net.milanqiu.sputnik;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * Charset filter.
+ * <p>
+ * Creation Date: 2020-02-07
+ * @author Milan Qiu
+ */
+public class CharsetFilter implements Filter {
+    public void destroy() {
+    }
+
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        resp.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/html;charset=UTF-8");
+
+        chain.doFilter(req, resp);
+    }
+
+    public void init(FilterConfig config) throws ServletException {
+
+    }
+
+}
--- sputnik-web/src/main/webapp/index.jsp
+++ sputnik-web/src/main/webapp/index.jsp
@@ -1,11 +1,19 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" pageEncoding="UTF-8" %>
 <html>
+<head>
+    <title>sputnik-web</title>
+</head>
 <body>
 <h2>sputnik-web</h2>
 <hr>
 This project was created from maven-archetype-webapp.<br><br>
+<form method="get" action="MainServlet">
+    <input type="text" name="param">
+    <input type="submit" value="submit by get">
+</form>
 <form method="post" action="MainServlet">
     <input type="text" name="param">
-    <input type="submit" value="submit">
+    <input type="submit" value="submit by post">
 </form>
 </body>
 </html>
